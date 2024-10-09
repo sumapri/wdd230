d40b89d8a35426bdd64975e180dd0d7d89710942
sumapri <114256701+sumapri@users.noreply.github.com>
2024-10-09 17:53:20 -0500

    still in progress

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,10 +1,19 @@
+function printYear(){    
+  const currentYear = new Date();
+  return currentYear.getFullYear();
+}
 
-document.addEventListener("DOMContentLoaded", function() {
-  function getCopyrightYear() {
-      const year = new Date().getFullYear();
-      return ` ${year}`;
-  }
-  document.getElementById("cYear").innerHTML = getCopyrightYear();
+document.getElementById("currentYear").innerHTML = printYear();
+
+function printLastModefied(){
+  const lastModefied = new Date();
+  let month = lastModefied.getMonth();
+  let day = lastModefied.getDate(); 
+  let year = lastModefied.getFullYear();
+  let timeFormated = lastModefied.toLocaleString('en-US', {hour: 'numeric', minute: 'numeric', second: 'numeric', hour12:true});
+  const dateFormated = month + "/" + day + "/" + year + " " + timeFormated;
   
-  
- 
+  return dateFormated;
+}
+
+document.getElementById("lastModified").innerHTML = printLastModefied();
