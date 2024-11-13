2422a5265d541d625a06051d4909d4746da1c7e6
sumapri <114256701+sumapri@users.noreply.github.com>
2024-11-12 22:49:23 -0500

    getdate2

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -3,7 +3,7 @@ const currentYear = new Date().getFullYear();
 document.getElementById('currentYear').textContent = currentYear;
 
 // Get the last modified date and populate the footer
-document.getElementById('lastModified').textContent = `lastModified: ${document.lastModified}`;
+document.getElementById('last-modified').textContent = `lastModified: ${document.lastModified}`;
 
 
 
